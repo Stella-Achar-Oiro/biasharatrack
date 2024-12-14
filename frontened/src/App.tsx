eaef88f8929851f451732de1d2940f8fad1935e2
Wendy-Tabitha <wendytbt4@gmail.com>
2024-12-14 16:10:06 +0300

    feat: add dashboard pages routes

--- frontened/src/App.tsx
+++ frontened/src/App.tsx
@@ -7,7 +7,12 @@ import Unauthorized from './pages/error/Unauthorized';
 import ServerError from './pages/error/ServerError';
 import ErrorBoundary from './homepage/components/ErrorBoundary';
 import FloatingLanguageSwitcher from './homepage/components/FloatingLanguageSwitcher';
-import React from 'react';
+import InventoryManagement from './pages/inventory/InventoryManagement';
+import SalesManagement from './pages/sales/SalesManagement';
+import CreditsManagement from './pages/credits/CreditsManagement';
+import Dashboard from './pages/Dashboard';
+import ProtectedRoute from './context/ProtectedRoute';
+import ReceiptManagement from './pages/receipts/ReceiptManagement';
 
 
 function App() {
@@ -19,7 +24,53 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          
+
+          <Route path="/unauthorized" element={<Unauthorized />} />
+          <Route path="/server-error" element={<ServerError />} />
+          <Route path="*" element={<NotFound />} />
+
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard/inventory"
+            element={
+              <ProtectedRoute>
+                <InventoryManagement />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard/sales"
+            element={
+              <ProtectedRoute>
+                <SalesManagement />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/dashboard/credits"
+            element={
+              <ProtectedRoute>
+                <CreditsManagement />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/dashboard/receipts"
+            element={
+              <ProtectedRoute>
+                <ReceiptManagement />
+              </ProtectedRoute>
+            }
+          />
           <Route path="/unauthorized" element={<Unauthorized />} />
           <Route path="/server-error" element={<ServerError />} />
           <Route path="*" element={<NotFound />} />
