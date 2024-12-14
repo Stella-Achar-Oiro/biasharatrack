3f032d81dd5b02c0affe80cc4e536eb5aa4b1cd6
Raymond9734 <rymadara97@gmail.com>
2024-12-14 12:53:02 +0300

    feat(models): add business details to Register and User models
    - Added BusinessName, Telephone, and Location fields to the Register struct
    - Updated User struct to include BusinessName, Telephone, and Location fields
    - Enhanced user model to support additional business-related information

--- backend/models/auth.go
+++ backend/models/auth.go
@@ -8,15 +8,21 @@ type AuthRequest struct {
 }
 
 type Register struct {
-	FullName string `json:"fullName"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	FullName     string `json:"fullName"`
+	Email        string `json:"email"`
+	Password     string `json:"password"`
+	BusinessName string `json:"businessName"`
+	Telephone    string `json:"telephone"`
+	Location     string `json:"location"`
 }
 type User struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	FullName  string    `gorm:"not null" json:"fullName"`
-	Email     string    `gorm:"type:varchar(255);uniqueIndex;not null" json:"email"`
-	Password  string    `gorm:"not null" json:"password"`
-	CreatedAt time.Time `gorm:"autoCreateTime" json:"createdAt"`
-	UpdatedAt time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
+	ID           uint      `gorm:"primaryKey" json:"id"`
+	FullName     string    `gorm:"not null" json:"fullName"`
+	Email        string    `gorm:"type:varchar(255);uniqueIndex;not null" json:"email"`
+	Password     string    `gorm:"not null" json:"password"`
+	BusinessName string    `gorm:"not null" json:"businessName"`
+	Telephone    string    `gorm:"not null" json:"telephone"`
+	Location     string    `gorm:"not null" json:"location"`
+	CreatedAt    time.Time `gorm:"autoCreateTime" json:"createdAt"`
+	UpdatedAt    time.Time `gorm:"autoUpdateTime" json:"updatedAt"`
 }
